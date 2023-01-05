fc1ef0e1f24798651b5ca3c01e8b1f4e7900444b
Ty Reid <trawx11@gmail.com>
2023-01-05 17:08:19 -0500

    book upload and on edit functions implemented c110

---
+++ src/components/BookEdit.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function BookEdit({ book, onEdit }) {
+const [title, setTitle] = useState(book.title);
+
+const handleChange = (event) => {
+    setTitle(event.target.value);
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log('New title:', title)
+}
+    return <form className="book-edit" onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input className="input" value={title} onChange={handleChange}/>
+        <button className="button is-primary">Save</button>
+        </form>;
+}
+
+export default BookEdit;
