77754bc9c3d8b30c3681bfe763ae689ec3b2289d
Ty Reid <trawx11@gmail.com>
2023-01-08 17:42:14 -0500

    seperated use books context hook

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,13 @@
 import BookShow from "./BookShow";
 import { useContext } from "react";
 import BooksContext from "../context/book";
+import useBooksContext from "../hooks/use-books-context";
+
+
 
 function BookList() {
 
-    const { books } =  useContext(BooksContext); 
+    const { books } =  useBooksContext(); 
    
     const renderedBooks = books.map((book) => {
         return <BookShow  key={book.id} book={book} />;
